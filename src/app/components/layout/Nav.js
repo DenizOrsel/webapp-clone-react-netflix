aa613684297288b3dad143b4b8fb1d77a0e51ae8
Deniz Orsel <denizonerorsel@gmail.com>
2022-10-07 10:47:53 +0200

    create homescreen

---
+++ src/app/components/layout/Nav.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, []);
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav__contents">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="Netflix Logo"
+          className="nav__logo"
+        />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSinkpaEjVFBcW2Qp3MUAlHf3FhFSeMs728b3epQ3J4K95o37SuG_WeFTCj0fPI9z0RVdE&usqp=CAU"
+          alt="Avatar"
+          className="nav__avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRmctBEElQ8vCql_qXQp400zg6x9vsr5w",
+  authDomain: "netflix-clone-99622.firebaseapp.com",
+  projectId: "netflix-clone-99622",
+  storageBucket: "netflix-clone-99622.appspot.com",
+  messagingSenderId: "375736838069",
+  appId: "1:375736838069:web:a20dbbc199e35a3fed9bfc",
+};
